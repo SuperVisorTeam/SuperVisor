663979151da3ec9f3b81688b5f98564f990a2c31
huanghuiliang <@home-pc>
2014-04-24 21:22:21 +0800

    重新Push

--- src/com/gdut/supervisor/ui/LoginActivity.java
+++ src/com/gdut/supervisor/ui/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.gdut.supervisor.ui;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
